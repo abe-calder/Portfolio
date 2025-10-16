0a40f40cd1b0208d28ad20ac7847d0ad920311e9
Abe Calder <abecalder1@gmail.com>
2025-10-16 15:47:22 +1300

    fix typos and add CapitaVault deployed site link

--- client/components/HomeBottom.tsx
+++ client/components/HomeBottom.tsx
@@ -26,69 +26,6 @@ export default function HomeBottom() {
           <Flex direction="column" gap="3">
 	          <Heading id="personal" style={{ borderBottom: '1px solid white', width: 'fit-content', alignSelf: 'center'}} color="sky" size="9">Personal</Heading>
           </Flex>
-        <Box className="project-box" maxWidth="15vw" style={{paddingTop: '3vh'}}>
-	        <Card  size="4">
-            <Inset   side="top" pb="current">
-              <img
-                src="/images/Screenshot 2025-09-23 205726.png"
-                alt="Bold typography"
-                style={{
-                  display: "block",
-                  objectFit: "cover",
-                  width: "16vw",
-                  height: '15vh',
-                }}
-              />
-            </Inset>
-            <Text as="p" size="3">
-              <Strong>Todos</Strong> This is 
-              a web app that was built as a 
-              place to store a list of things 
-                that I needed to do. Todos was build using React TypeScript
-                and Express SQLite for back-end.
-                <br></br>
-                <br></br>
-                I learnt the foundations of
-                databases and database structure
-                during this project. And reinforced 
-                my knowledge of REST api CRUD
-                operations.
-                <br></br>
-              
-              <a style={{color: 'maroon'}} href="https://todo-full-stack-jggn.onrender.com/">Delopyed Site </a> | 
-              <a style={{color: 'green'}} href="https://github.com/abe-calder/TODO-fullstack-database"> GitHub Repo</a>
-            </Text>
-          </Card>
-          </Box>
-          <Box className="project-box" maxWidth="15vw" style={{paddingTop: '3vh'}}>
-	        <Card  size="4">
-            <Inset   side="top" pb="current">
-              <img
-                src="/images/cr-site-player-stats.png"
-                alt="Bold typography"
-                style={{
-                  display: "block",
-                  objectFit: "cover",
-                  width: "16vw",
-                  height: '15vh',
-                }}
-              />
-            </Inset>
-            <Text as="p" size="3">
-                <Strong>Cr-Site</Strong> This is a web app that I
-                have build to solve a probelm I had where I couldn&apos;t see
-                the in game statistics that I wanted, for a
-                video game I play.
-                Using their offical API, CR-Site was build with React
-                TypeScript, React router, useQuery,
-                useMutation, and many more
-                technologies.
-                <br></br>
-              <a style={{color: 'maroon'}} href="https://cr-site.onrender.com">Delopyed Site </a> | 
-              <a style={{color: 'green'}} href="https://github.com/abe-calder/CR-Site"> GitHub Repo</a>
-            </Text>
-          </Card>
-          </Box>
           <Box className="project-box" maxWidth="15vw" style={{paddingTop: '3vh'}}>
 	        <Card  size="4">
             <Inset   side="top" pb="current">
@@ -104,24 +41,83 @@ export default function HomeBottom() {
               />
             </Inset>
             <Text as="p" size="3">
-                <Strong>CapitaVault</Strong> is a website that I am
-                currently in the process of building.
-                CapitaVault will provide a place to store any assets
-                (Crypto/Stocks), and then have
-                their value displayed back to the
-                user.
+                <Strong>CapitaVault</Strong> is my latest and favorite project
+                that has taken my React TypeScript skills to the next level.
+                In this project, I have learned to incorporate useContext,
+                useMemo, and custom providers.
                 <br></br>
                 <br></br>
-                This is done using the Polygon API. It is being build with
-                React TypeScript, React router,
-                useQuery, useEffect, Websocket,
-                and many more technologies.
-                
+                I have found these technologies to be crucial for improving
+                load times, as well as ensuring data can be moved across
+                components effectively and efficiently.
                 <br></br>
+                <br></br>
+                This website is built using the Polygon API and fxRates API.
+                Some technologies it is built with are React, TypeScript, React Router, useQuery,
+                useEffect, WebSocket, useContext, useMemo, and many more.
+                <br></br>
+              <a style={{color: 'maroon'}} href="https://capitavault.onrender.com/">Delopyed Site </a> | 
               <a style={{color: 'green'}} href="https://github.com/abe-calder/CapitaVault"> GitHub Repo</a>
             </Text>
           </Card>
         </Box>
+          <Box className="project-box" maxWidth="15vw" style={{paddingTop: '3vh'}}>
+	        <Card  size="4">
+            <Inset   side="top" pb="current">
+              <img
+                src="/images/cr-site-player-stats.png"
+                alt="Bold typography"
+                style={{
+                  display: "block",
+                  objectFit: "cover",
+                  width: "16vw",
+                  height: '15vh',
+                }}
+              />
+            </Inset>
+            <Text as="p" size="3">
+                <Strong>Cr-Site</Strong> is a web app that I have built to solve
+                a problem I had where I couldn&apos;t see the in-game statistics
+                that I wanted for a mobile game that I play.
+                <br></br>
+                <br></br>
+                Using Clash Royale&apos;s official API, CR-Site was built with React, TypeScript,
+                React Router, useQuery, useMutation, and many more technologies.
+                <br></br>
+              <a style={{color: 'maroon'}} href="https://cr-site.onrender.com">Delopyed Site </a> | 
+              <a style={{color: 'green'}} href="https://github.com/abe-calder/CR-Site"> GitHub Repo</a>
+            </Text>
+          </Card>
+          </Box>
+          <Box className="project-box" maxWidth="15vw" style={{paddingTop: '3vh'}}>
+	        <Card  size="4">
+            <Inset   side="top" pb="current">
+              <img
+                src="/images/Screenshot 2025-09-23 205726.png"
+                alt="Bold typography"
+                style={{
+                  display: "block",
+                  objectFit: "cover",
+                  width: "16vw",
+                  height: '15vh',
+                }}
+              />
+            </Inset>
+            <Text as="p" size="3">
+                <Strong>Todos</Strong> is a web app that was built as a place to store a list
+                of things that I needed to do. Todos was built using React and TypeScript
+                on the front-end, and Express with SQLite for the back-end.
+                <br></br>
+                <br></br>
+                Through this project, I learned the foundations of databases
+                and database structure, and reinforced my knowledge
+                of REST API CRUD operations.
+                <br></br>
+              <a style={{color: 'maroon'}} href="https://todo-full-stack-jggn.onrender.com/">Delopyed Site </a> | 
+              <a style={{color: 'green'}} href="https://github.com/abe-calder/TODO-fullstack-database"> GitHub Repo</a>
+            </Text>
+          </Card>
+          </Box>
       </div>
       <div aria-hidden="true"
         className="hidden sm:absolute sm:-middle sm:left-[calc(66.66%-4vw)] sm:-z-10 sm:mr-10 sm:block sm:transform-gpu">
@@ -144,22 +140,15 @@ export default function HomeBottom() {
               />
             </Inset>
             <Text as="p" size="3">
-              <Strong>Whats Up</Strong> Is a browser messaging 
-              app that my colleagues and I 
-              worked together on during our 
-              final group project. It includes 
-                Auth0 integrated sign-in/out.
-                Whats Up was build using React 
-                TypeScript, React router, Express
-                server, SQLite database, tailwind
+                <Strong>Whats Up</Strong> Whats Up is a browser messaging app that my
+                colleagues and I worked together on during our final group project.
+                It includes Auth0-integrated sign-in/out. Whats Up was built using React,
+                TypeScript, React Router, an Express server, SQLite database, Tailwind,
                 and many more technologies.
                 <br></br>
                 <br></br>
-                This project reinfoced my team
-                and collaboration skills and
-                fostered a great enviroment for me
-                to learn how to implement a
-                WebSocket.
+                This project reinforced my team and collaboration skills and fostered
+                a great environment for me to learn how to implement a WebSocket.
                 <br></br>
               <a style={{color: 'maroon'}} href="https://whats-up-zh1w.onrender.com/">Delopyed Site </a> | 
               <a style={{color: 'green'}} href="https://github.com/hotoke-2025/whats-up"> GitHub Repo</a>
